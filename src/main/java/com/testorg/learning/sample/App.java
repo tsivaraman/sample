b8068093f367b30961ecdf07c516377d4fe9a5bf
Sivaraman Thiagarajan -X (sivthiag - ACCENTURE LLP at Cisco) <sivthiag@cisco.com>
2018-08-06 14:53:20 +0530

    First Commit

---
+++ src/main/java/com/testorg/learning/sample/App.java
@@ -0,0 +1,22 @@
+package com.testorg.learning.sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        if (args[0].equalsIgnoreCase("1"))
+        {
+        	System.out.println(">>>>>> Option 1");
+        }
+        if (args[0].equalsIgnoreCase("2"))
+        {
+        	System.out.println(">>>>>> Option 2");
+        }
+        
+    }
+}
